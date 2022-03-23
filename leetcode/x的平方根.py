69053259b2da76a6aa3d991d894809d73e08e4ce
LawsonAbs <shenliu@ahnu.edu.cn>
2022-03-23 10:39:45 +0800

    high和low的变化两者都是要变的，否则会造成死循环

--- leetcode/x的平方根.py
+++ leetcode/x的平方根.py
@@ -1,3 +1,6 @@
+'''
+二分法中，如果high和low只有一个变化，那么很有可能就无法跳出循环。所以这里 一般的都是 high = mid-1, low = mid+ 1
+'''
 class Solution:
     def mySqrt(self, x: int) -> int:        
         if x == 0: # 需要注意特殊用例
@@ -8,7 +11,9 @@ class Solution:
             mid = (low+high) // 2
             if x/mid < mid : 
                 high = mid-1 # 不可能取mid
-            elif x/mid > mid:
+            
+            # 这里如果 取low = mid+1 很有可能就错过答案了。所以后面再来一个if-else 进行判断
+            elif x/mid > mid: 
                 low = mid+1
             elif x/mid == mid:
                 return mid
